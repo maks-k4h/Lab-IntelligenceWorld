95bdae5b69c8e70ed7cde4f8760f6110028a5d7f
Maksym Konevych <konevych.m.m@gmail.com>
2023-05-20 13:57:39 +0300

    feat(Style): change bootstrap theme

--- Lab2-IntelligenceAgencies/Controllers/HomeController.cs
+++ Lab2-IntelligenceAgencies/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Menu()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
